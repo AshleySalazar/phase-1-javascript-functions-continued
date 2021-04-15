3e5d42a9a3de4bda844b06f74af8097ec5048962
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-04-15 18:30:38 -0500

    commit tree 883ed48768b15ef3668440a1616bf9a4223d8a3c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+};
+
+ const mondayWork = function(bills="go to the office"){
+    return `This Monday, I will ${bills}.`
+ }
+
+ function wrapAdjective(salty="*"){
+     return function(xavier="special") {
+         return `You are ${salty}${xavier}${salty}!`
+
+     }
+    
+ }
